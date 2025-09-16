10ea18419f6916d8c6fa92f6a737e7f541a2b414
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-16 00:35:37 -0400

    Unenrollment

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,13 +9,25 @@ export const collections = {
         discord: z.string().optional(),
         "official-link": z.string().optional(),
         kernver: z.number().optional(),
+        unenrollment: z.boolean().optional(),
       }),
     }),
   }),
 };
 
 export async function getkajigs() {
-  const kajigs = ( await getCollection("docs")).filter((doc) => doc.id.startsWith("kajigs/") && doc.id !== "kajigs" && doc.data.kernver !== undefined );
+  const kajigs = ( await getCollection("docs")).filter((doc) => doc.id.startsWith("kajigs/") && doc.data.kernver !== undefined );
+  const kernver: Record<number, { title: string; slug: string }[]> = {};
+  for (const doc of kajigs) {
+    const key = doc.data.kernver!;
+    if (!kernver[key]) kernver[key] = [];
+    kernver[key].push({ title: doc.data.title, slug: `/${doc.slug}` });
+  }
+  return kernver;
+}
+
+export async function getunenrollments() {
+  const kajigs = ( await getCollection("docs")).filter((doc) => doc.id.startsWith("kajigs/") && doc.data.kernver !== undefined  && doc.data.unenrollment === true );
   const kernver: Record<number, { title: string; slug: string }[]> = {};
   for (const doc of kajigs) {
     const key = doc.data.kernver!;
