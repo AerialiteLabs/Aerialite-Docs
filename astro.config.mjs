242d95a3aed61fe34d142e315606f25b09d87ca7
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-16 01:06:10 -0400

    bye houston

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Aerialite Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/AerialiteLabs/Aerialite-Docs' }],
 			sidebar: [
 				{
 					label: 'Kajigs',
