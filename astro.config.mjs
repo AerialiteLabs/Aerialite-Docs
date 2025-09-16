07fa4399fcb26a4956bfb65fd592707bc3f2ce3e
Sophia <193290223+soap-phia@users.noreply.github.com>
2025-09-16 10:16:25 -0400

    unenrollment, extensions, favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
+      		favicon: "/favicon.png",
 			title: 'Aerialite Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/AerialiteLabs/Aerialite-Docs' }],
 			sidebar: [
